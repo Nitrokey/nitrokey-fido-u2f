c165ef144987ae254a5124ec0f82c86bd8e6c98e
Szczepan Zalega <szczepan@nitrokey.com>
2018-09-14 13:46:46 +0200

    Read constants directly from EEPROM
    Signed-off-by: Szczepan Zalega <szczepan@nitrokey.com>

--- firmware/src/atecc508a.c
+++ firmware/src/atecc508a.c
@@ -882,9 +882,8 @@ void atecc_setup_device(struct config_msg * usb_msg_in)
 #ifndef _PRODUCTION_RELEASE
 		case U2F_CONFIG_GET_CONSTANTS:
 			usb_msg_out.buf[0] = ASD_ERR_OTHER;
-
-			memmove(usb_msg_out.buf+1, device_configuration.WMASK, 16);
-			memmove(usb_msg_out.buf+1+16, device_configuration.RMASK, 16);
+			eeprom_read(EEPROM_DATA_WMASK, usb_msg_out.buf+1, 16);
+			eeprom_read(EEPROM_DATA_RMASK, usb_msg_out.buf+1+16, 16);
 			eeprom_read(EEPROM_DATA_U2F_CONST, usb_msg_out.buf+1+16+16, 16);
 
 			usb_msg_out.buf[0] = ASD_ERR_SUCCESS;
