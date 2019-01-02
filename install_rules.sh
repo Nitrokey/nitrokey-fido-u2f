ccc73ffaf67a77677375be553e154f5746b431c9
Szczepan Zalega <szczepan@nitrokey.com>
2019-01-02 15:35:23 +0100

    Make the udev installation script verbose, and enable cp backup
    Signed-off-by: Szczepan Zalega <szczepan@nitrokey.com>

--- install_rules.sh
+++ install_rules.sh
@@ -1,2 +1,13 @@
-sudo cp ./70-u2f.rules /etc/udev/rules.d/ -v
-sudo udevadm control --reload-rules && udevadm trigger
+#!/bin/bash
+echo
+echo "Install Nitrokey FIDO U2F Udev rules file on Ubuntu-like OS."
+echo
+echo "*** Copy rules file and restart udev service"
+set -x
+
+sudo cp ./70-u2f.rules /etc/udev/rules.d/ -v --backup
+sudo udevadm control --reload-rules && sudo udevadm trigger
+
+set +x
+echo "*** Finished"
+echo
